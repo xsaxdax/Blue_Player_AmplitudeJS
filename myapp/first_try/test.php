ff244475a5d8e3e882401a125d7fd5af70a7d506
xsaxdax <60689251+xsaxdax@users.noreply.github.com>
2021-01-14 08:59:39 +0100

    x

---
+++ myapp/first_try/test.php
@@ -0,0 +1,9 @@
+
+<?php
+$path_parts = pathinfo('/www/htdocs/inc/lib.inc.php');
+
+echo $path_parts['dirname'], "\n";
+echo $path_parts['basename'], "\n";
+echo $path_parts['extension'], "\n";
+echo $path_parts['filename'], "\n"; // seit PHP 5.2.0
+?>
