23828b7f0f9632fe0a32bb410154e45bc66d09c3
xsaxdax <60689251+xsaxdax@users.noreply.github.com>
2021-04-16 15:50:56 +0200

    First Working Version of the ract port

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Init from './Init'
+import Player from './Player';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Init/>
+    <Player/>
   </React.StrictMode>,
   document.getElementById('root')
 );
